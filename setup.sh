385068f588d8485af6abea651cc0c7ca7780530c
Nick Fox-Gieg <nick@fox-gieg.com>
2020-03-16 09:17:23 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,6 @@ cd $DIR
 
 cd bin/data
 
-FILEID=1FBbw7X4hdx9Or91A4ObPN1zhqixssRK3
 FILENAME=Comp_8.mp4
 
-#wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
-#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=$FILEID' -O $FILENAME
+wget https://fox-gieg.com/stuff/rpi/$FILENAME
